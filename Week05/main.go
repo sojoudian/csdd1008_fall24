114b572d3c1356f8e8f7cd1ce4cd5bc58771119b
maziar <sojodyan@gmail.com>
2024-10-03 17:46:30 -0400

    add get API

--- Week05/main.go
+++ Week05/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+type Message struct {
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Welcome to Go API!")
+}
+
+func main() {
+	// p()
+	http.HandleFunc("/get", getHandler)
+	http.ListenAndServe(":8090", nil)
 }
