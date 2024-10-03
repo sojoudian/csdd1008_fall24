f59ca1cc1a8aff1737477362a36b0e0d63dffcd4
maziar <sojodyan@gmail.com>
2024-10-03 18:22:04 -0400

    add post

--- Week05/main.go
+++ Week05/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
@@ -18,8 +21,33 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to Go API!")
 }
 
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	var msg Message
+	// Read body of the POST request
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Invalid read request body", http.StatusBadRequest)
+		return
+	}
+	// Parse the JSON
+	err = json.Unmarshal(body, &msg)
+	if err != nil {
+		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+		return
+	}
+	// Response back with the same MSG
+	w.Header().Set("Content/Type", "application/json")
+	json.NewEncoder(w).Encode(msg)
+}
+
 func main() {
 	// p()
 	http.HandleFunc("/get", getHandler)
-	http.ListenAndServe(":8090", nil)
+	http.HandleFunc("/post", postHandler)
+	fmt.Println("Server running on port: 8090...")
+	log.Fatal(http.ListenAndServe(":8090", nil))
 }
