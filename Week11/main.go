bc125c2ecd114f49ae9ec30129015d091a6f3406
maziar <sojodyan@gmail.com>
2024-11-14 18:04:45 -0500

    add main

--- Week11/main.go
+++ Week11/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func Add(a, b int) int {
+	return a + b
+}
+
+func Subtract(a, b int) int {
+	return a - b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
+
+func main() {
+	fmt.Println("Addition: ", Add(2, 3))
+	fmt.Println("Subtraction: ", Subtract(4, 2))
+	fmt.Println("Multiplication: ", Multiply(2, 4))
 }
