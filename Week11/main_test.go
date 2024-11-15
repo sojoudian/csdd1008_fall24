f5416422cd8cc66ea9db3ae48a210b57b6b5ea16
maziar <sojodyan@gmail.com>
2024-11-15 08:23:06 -0500

    update repo

--- Week11/main_test.go
+++ Week11/main_test.go
@@ -4,28 +4,31 @@ import "testing"
 
 // Test for Add function
 func TestAdd(t *testing.T) {
-    result := Add(2, 3)
-    expected := 5
-    if result != expected {
-        t.Errorf("Add(2, 3) = %d; expected %d", result, expected)
-    }
+	result := Add(2, 3)
+	expected := 5
+	if result != expected {
+		t.Errorf("Add(2, 3) = %d; expected %d", result, expected)
+	}
 }
 
 // Test for Subtract function
 func TestSubtract(t *testing.T) {
-    result := Subtract(5, 3)
-    expected := 2
-    if result != expected {
-        t.Errorf("Subtract(5, 3) = %d; expected %d", result, expected)
-    }
+	result := Subtract(5, 3)
+	expected := 2
+	if result != expected {
+		t.Errorf("Subtract(5, 3) = %d; expected %d", result, expected)
+	}
 }
 
 // Test for Multiply function
 func TestMultiply(t *testing.T) {
-    result := Multiply(2, 3)
-    expected := 6
-    if result != expected {
-        t.Errorf("Multiply(2, 3) = %d; expected %d", result, expected)
-    }
+	result := Multiply(2, 3)
+	expected := 6
+	if result != expected {
+		t.Errorf("Multiply(2, 3) = %d; expected %d", result, expected)
+	}
 }
 
+func BenchmarkMultiply(t *testing.B) {
+
+}
